7b0c72853ec7ed66dc6ac3fc290bd6fd1b8c05cf
Dhina-S <dhinaselvaraj30@gmail.com>
2026-06-30 00:17:13 +0530

    feat: Add Patients With a Condition

--- Database/Patients With a Condition/sql/solution.sql
+++ Database/Patients With a Condition/sql/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select patient_id,patient_name,conditions
 from patients
-where conditions like 'DIAB100%' or conditions like '% DIAB100';
+where conditions like 'DIAB1%' or conditions like '% DIAB1';
